27d7f2e4a5199f61e2cf520de1270b55f4d7e4aa
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-06-29 00:15:33 +0200

    adding auth client and working with middlewarets

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,9 @@ import { PrismaClient } from "@/lib/generated/prisma/client";
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!signin|signup|api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
